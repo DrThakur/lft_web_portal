7da97434d24f4a0ee00e9ee404aaf068df47f97b
Piul Dolai <dolaipipul014@gmail.com>
2025-02-18 16:25:47 +0530

    Fix: Resolve pusches order save border color blue  set, improve mobile view adjustments

--- src/Components/PurchaseOrder.jsx
+++ src/Components/PurchaseOrder.jsx
@@ -714,7 +714,7 @@ const PurchaseOrder = () => {
           </button>
           <button
             type="submit"
-            className="bg-blue-500 text-white rounded px-4 py-2 hover:bg-blue-600 border-2 w-full sm:w-1/4"
+            className="bg-blue-500 text-white border-2 border-blue-500 rounded px-4 py-2 hover:bg-blue-600  w-full sm:w-1/4"
           >
             Save
           </button>
